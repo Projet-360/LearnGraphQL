71778fe06bfda78c61169f0bfddcc03abf0208cc
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2022-11-12 16:57:30 +0100

    ALL DONE

--- src/views/search/index.tsx
+++ src/views/search/index.tsx
@@ -9,39 +9,64 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
+import  TablePaginationActions from "../utils/manageTable";
+
 import { LIST_LAZY } from "../../api/gql/lazyQueries";
 import { useLazyQuery } from "@apollo/client";
 
 export default function CustomPaginationActionsTable() {
-const [name, setName] = useState('');
-const [getData, { error, loading, data, called }] = useLazyQuery(LIST_LAZY, {
-  variables: {
-    name,
-  }
-});
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [name, setName] = useState('');
+  const [getData, { error, loading, data, called }] = useLazyQuery(LIST_LAZY, {
+    variables: {
+      name,
+    }
+  });
 
-useEffect(() => {
+  useEffect(() => {
 
-getData({
-  variables: {
-    name,
-  }
-})
+    getData({
+      variables: {
+        name,
+      }
+    })
+  
+    }, [name]);
 
-}, [name]);
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data?.characters.results.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number,
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
 return (
 <>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <button onClick={()=> getData()}>Rechercher</button>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-          <TableBody>
-            { !loading && data &&
-            data?.characters.results.map((row) => (
+    <input type="text" value={name} placeholder="" onChange={(e) => setName(e.target.value)} />
+    
+    { !loading && data &&
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+        <TableBody>
+          {(rowsPerPage > 0
+            ? data?.characters.results.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : data?.characters.results
+          ).map((row) => (
             <TableRow key={row.id}>
               <TableCell component="th" scope="row">
-                <img src={row.image} style={{ width: '100px' }} />
+                <img src={row.image} style={{ width: '100px' }}/>
               </TableCell>
               <TableCell component="th" scope="row">
                 {row.name}
@@ -56,11 +81,36 @@ return (
                 {row.type}
               </TableCell>
             </TableRow>
-            ))
-            }
-          </TableBody>
-        </Table>
-      </TableContainer>
+          ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={data?.characters.results.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+    }
 </>
 );
 }
