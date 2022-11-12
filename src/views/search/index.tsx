4641ba9519b35634f4c44710c48c8b36b8c6bd2d
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2022-11-12 16:33:19 +0100

    Search DONE

--- src/views/search/index.tsx
+++ src/views/search/index.tsx
@@ -9,69 +9,39 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
-
 import { LIST_LAZY } from "../../api/gql/lazyQueries";
 import { useLazyQuery } from "@apollo/client";
-import  TablePaginationActions from "../utils/manageTable";
 
 export default function CustomPaginationActionsTable() {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const [name, setName] = useState('');
-  const [getData, { error, loading, data }] = useLazyQuery(LIST_LAZY);
+const [name, setName] = useState('');
+const [getData, { error, loading, data, called }] = useLazyQuery(LIST_LAZY, {
+  variables: {
+    name,
+  }
+});
 
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data?.characters.results.length) : 0;
+useEffect(() => {
 
-  const handleChangePage = (
-    event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number,
-  ) => {
-    setPage(newPage);
-  };
+getData({
+  variables: {
+    name,
+  }
+})
 
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+}, [name]);
 
-  useEffect(() => {
-
-    getData({
-      variables: {
-        name,
-      }
-    })
-    
-  }, [name]);
-
-  console.log(data?.characters.results);
-  
-  return (
-    <>
-    {/* <Autocomplete
-    disablePortal
-    id="combo-box-demo"
-    options={data?.characters.results.map((option) => option.name)}
-    sx={{ width: 300 }}
-    renderInput={(params) => <TextField {...params} value={name} onChange={(e) => setName(e.target.value)} label="Personnages" />}
-  /> */}
-    { !loading && data &&
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
-        <TableBody>
-          {(rowsPerPage > 0
-            ? data?.characters.results.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : data?.characters.results
-          ).map((row) => (
-            <TableRow key={row.name}>
+return (
+<>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={()=> getData()}>Rechercher</button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+          <TableBody>
+            { !loading && data &&
+            data?.characters.results.map((row) => (
+            <TableRow key={row.id}>
               <TableCell component="th" scope="row">
-                <img src={row.image} style={{ width: '100px' }}/>
+                <img src={row.image} style={{ width: '100px' }} />
               </TableCell>
               <TableCell component="th" scope="row">
                 {row.name}
@@ -86,36 +56,11 @@ export default function CustomPaginationActionsTable() {
                 {row.type}
               </TableCell>
             </TableRow>
-          ))}
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={3}
-              count={data?.characters.results.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  'aria-label': 'rows per page',
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
-    }
-    </>
-  );
+            ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
+</>
+);
 }
